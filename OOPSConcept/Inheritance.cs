8263c1b65126c290c88c1a42499c9d7811d8df5a
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-13 17:36:11 +0530

    [Ritvik]Add: Inheritance Types

--- OOPSConcept/Inheritance.cs
+++ OOPSConcept/Inheritance.cs
@@ -24,4 +24,78 @@ namespace OOPSConcept
         public string CompanyName { get; set; }
     }
 
+    // -- MultiLevel Inheritance
+
+    public class Manager : Employee
+    {
+        public int ManagerId { get; set; }
+
+    }
+
+    // -- Hierarchical Inheritance
+    public class Supervisor : Person
+    {
+        private int SupervisorId;
+
+        public int GetSupervisorId
+        {
+            get { return SupervisorId; }
+            set { SupervisorId = value; }
+        }
+    }
+
+    // -- Multiple Inheritance using interface
+
+    public interface ICustomer
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+
+        decimal productAmount { get; set; }
+
+        decimal CalculateDiscount();
+    }
+
+    public interface ICustomerGreeting : ICustomer
+    {
+        string GreetingMessage();
+    }
+
+    public abstract class Customer : ICustomer, ICustomerGreeting
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public decimal productAmount { get; set; }
+        public abstract decimal CalculateDiscount();
+
+        public abstract string GreetingMessage();
+    }
+
+    public class GoldCustomer : Customer
+    {
+        public override decimal CalculateDiscount()
+        {
+            return productAmount - 10;
+        }
+
+        public override string GreetingMessage()
+        {
+            return "Thank You ;-) ";
+        }
+    }
+
+    public class SilverCustomer : Customer
+    {
+        public override decimal CalculateDiscount()
+        {
+            return productAmount - 20;
+        }
+
+        public override string GreetingMessage()
+        {
+            return " Thank you. Visit again.";
+        }
+    }
+
 }
--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -23,13 +23,53 @@
 
             // -- Single Level Inheritance
 
-            Employee employee = new Employee();
-            employee.FirstName = "Ankur";
-            employee.LastName = "Bhardhwaj";
-            employee.EmployeeId = 1;
-            employee.CompanyName = "Radha Textiles";
-            Console.WriteLine("Employee Full Name is " + employee.FirstName + " " + employee.LastName +
-                            " and employee ID is " + employee.EmployeeId + " for the company " + employee.CompanyName + ".\n");
+            //Employee employee = new Employee();
+            //employee.FirstName = "Ankur";
+            //employee.LastName = "Bhardhwaj";
+            //employee.EmployeeId = 1;
+            //employee.CompanyName = "Radha Textiles";
+            //Console.WriteLine("Employee Full Name is " + employee.FirstName + " " + employee.LastName +
+            //                " and employee ID is " + employee.EmployeeId + " for the company " + employee.CompanyName + ".\n");
+
+            // --Multi Level Inheritance
+
+            Manager manager = new Manager();
+
+            manager.FirstName = "Saurav";
+            manager.LastName = "Chaudhary";
+
+            manager.CompanyName = "Radha Textiles";
+            manager.ManagerId = 100;
+
+            Console.WriteLine("Manager Full Name is " + manager.GetFullName() + " and mangerID is " +
+                                manager.ManagerId + " for the company " + manager.CompanyName + "\n");
+
+            // -- Hierarchical Interface
+
+            Supervisor supervisor = new Supervisor();
+            supervisor.GetSupervisorId = 25;
+            supervisor.FirstName = "Rishi";
+            supervisor.LastName = "Kapoor";
+            Console.WriteLine("Supervisor id is: " + supervisor.GetSupervisorId + " and full name is " + supervisor.GetFullName()+"\n");
+            
+
+            // Interface implemnetation (multiple inheritance)
+
+            GoldCustomer obj = new GoldCustomer();
+            obj.Name = "Ramesh";
+            obj.Address = "Bangalore";
+            obj.productAmount = 100;
+
+
+            Console.WriteLine("Name = " + obj.Name + "\nAddress = " + obj.Address + "\nProductAmount = " + obj.productAmount +
+                                        "\nProductAmount after discount = " + obj.CalculateDiscount() + "\nGreetingMessage = " + obj.GreetingMessage());
+
+            SilverCustomer obj2 = new SilverCustomer();
+            obj2.Name = "Suresh";
+            obj2.Address = "Bangalore";
+            obj2.productAmount = 100;
+            Console.WriteLine("\nName = " + obj2.Name + "\nAddress = " + obj2.Address + "\nProductAmount = " + obj2.productAmount +
+                                        "\nProductAmount after discount = " + obj2.CalculateDiscount() + "\nGreetingMessage = " + obj2.GreetingMessage());
 
             Console.Read();
         }
