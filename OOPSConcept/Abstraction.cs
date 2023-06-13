5944ca10720d06f739aeecb7a1ac7f080d0ab5de
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-13 18:08:35 +0530

    [Ritvik]Add: Abstraction through abstract class

---
+++ OOPSConcept/Abstraction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    // -- Abstraction is achived though abstract keyword
+
+    public abstract class Customer1
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public decimal productAmount { get; set; }
+        public abstract decimal CalculateDiscount();
+
+        public abstract string GreetingMessage();
+    }
+
+    public class SadCustomer : Customer1
+    {
+        public override decimal CalculateDiscount()
+        {
+            return productAmount - 10;
+        }
+
+        public override string GreetingMessage()
+        {
+            return "Thank You ;-) ";
+        }
+    }
+
+    public class HappyCustomer : Customer1
+    {
+        public override decimal CalculateDiscount()
+        {
+            return productAmount - 20;
+        }
+
+        public override string GreetingMessage()
+        {
+            return " Thank you. Visit again.";
+        }
+    }
+}
--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -33,45 +33,64 @@
 
             // --Multi Level Inheritance
 
-            Manager manager = new Manager();
+            //Manager manager = new Manager();
 
-            manager.FirstName = "Saurav";
-            manager.LastName = "Chaudhary";
+            //manager.FirstName = "Saurav";
+            //manager.LastName = "Chaudhary";
 
-            manager.CompanyName = "Radha Textiles";
-            manager.ManagerId = 100;
+            //manager.CompanyName = "Radha Textiles";
+            //manager.ManagerId = 100;
 
-            Console.WriteLine("Manager Full Name is " + manager.GetFullName() + " and mangerID is " +
-                                manager.ManagerId + " for the company " + manager.CompanyName + "\n");
+            //Console.WriteLine("Manager Full Name is " + manager.GetFullName() + " and mangerID is " +
+            //                    manager.ManagerId + " for the company " + manager.CompanyName + "\n");
 
-            // -- Hierarchical Interface
+            //// -- Hierarchical Interface
 
-            Supervisor supervisor = new Supervisor();
-            supervisor.GetSupervisorId = 25;
-            supervisor.FirstName = "Rishi";
-            supervisor.LastName = "Kapoor";
-            Console.WriteLine("Supervisor id is: " + supervisor.GetSupervisorId + " and full name is " + supervisor.GetFullName()+"\n");
+            //Supervisor supervisor = new Supervisor();
+            //supervisor.GetSupervisorId = 25;
+            //supervisor.FirstName = "Rishi";
+            //supervisor.LastName = "Kapoor";
+            //Console.WriteLine("Supervisor id is: " + supervisor.GetSupervisorId + " and full name is " + supervisor.GetFullName()+"\n");
             
 
-            // Interface implemnetation (multiple inheritance)
+            //// Interface implemnetation (multiple inheritance)
 
-            GoldCustomer obj = new GoldCustomer();
-            obj.Name = "Ramesh";
-            obj.Address = "Bangalore";
-            obj.productAmount = 100;
+            //GoldCustomer obj = new GoldCustomer();
+            //obj.Name = "Ramesh";
+            //obj.Address = "Bangalore";
+            //obj.productAmount = 100;
 
 
-            Console.WriteLine("Name = " + obj.Name + "\nAddress = " + obj.Address + "\nProductAmount = " + obj.productAmount +
-                                        "\nProductAmount after discount = " + obj.CalculateDiscount() + "\nGreetingMessage = " + obj.GreetingMessage());
+            //Console.WriteLine("Name = " + obj.Name + "\nAddress = " + obj.Address + "\nProductAmount = " + obj.productAmount +
+            //                            "\nProductAmount after discount = " + obj.CalculateDiscount() + "\nGreetingMessage = " + obj.GreetingMessage());
 
-            SilverCustomer obj2 = new SilverCustomer();
+            //SilverCustomer obj2 = new SilverCustomer();
+            //obj2.Name = "Suresh";
+            //obj2.Address = "Bangalore";
+            //obj2.productAmount = 100;
+            //Console.WriteLine("\nName = " + obj2.Name + "\nAddress = " + obj2.Address + "\nProductAmount = " + obj2.productAmount +
+            //                            "\nProductAmount after discount = " + obj2.CalculateDiscount() + "\nGreetingMessage = " + obj2.GreetingMessage());
+
+            //Console.Read();
+
+            // Abstraction
+
+            SadCustomer obj1 = new SadCustomer();
+            obj1.Name = "Sumit";
+            obj1.Address = "Bangalore";
+            obj1.productAmount = 100;
+
+            Console.WriteLine("Name = " + obj1.Name + "\nAddress = " + obj1.Address + "\nProductAmount = " + obj1.productAmount +
+                                        "\nProductAmount after discount = " + obj1.CalculateDiscount() + "\nGreetingMessage = " + obj1.GreetingMessage());
+
+            HappyCustomer obj2 = new HappyCustomer();
             obj2.Name = "Suresh";
             obj2.Address = "Bangalore";
             obj2.productAmount = 100;
             Console.WriteLine("\nName = " + obj2.Name + "\nAddress = " + obj2.Address + "\nProductAmount = " + obj2.productAmount +
                                         "\nProductAmount after discount = " + obj2.CalculateDiscount() + "\nGreetingMessage = " + obj2.GreetingMessage());
 
-            Console.Read();
+
         }
     }
 }
