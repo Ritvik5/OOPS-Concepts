5f8fb10609a9166cad4737e3413ff8c002865c87
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-13 17:24:49 +0530

    [Ritvik]Add: Encapsulation(Hiding Complex Information)

---
+++ OOPSConcept/Encapsulation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    public class Patient
+    {
+        private string Name { get; set; }
+
+        private string Address { get; set; }
+
+        public string GetName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public string GetAddress
+        {
+            get { return Address; }
+            set { Address = value; }
+        }
+    }
+
+    public class BankAccount
+    {
+        private decimal balance;
+
+        public BankAccount(decimal initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (balance >= amount)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Funds..");
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+}
--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Object Oriented Programming!!!");
+            Console.WriteLine("Welcome to Object Oriented Programing!!!\n");
+            Patient patient = new Patient();
+            patient.GetName = "Ajay";
+            patient.GetAddress = "127.0.0.1";
+
+            Console.WriteLine("Name of the Patient is: " + patient.GetName + " and Address is: " + patient.GetAddress + "\n");
+
+            BankAccount obj = new BankAccount(1000);
+            obj.Deposit(2000);
+
+            Console.WriteLine("Balance after deposit is: " + obj.GetBalance() + "\n");
+
+            obj.Withdraw(500);
+            Console.WriteLine("Balance after withdraw is: " + obj.GetBalance());
+            Console.Read();
         }
     }
 }
