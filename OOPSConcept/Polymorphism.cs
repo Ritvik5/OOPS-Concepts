7d869097adf7e1c17053158bd3ea7bede0ddef5f
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-13 18:56:03 +0530

    [Ritvik]Add: Polymorphism by Method Overloading and Overriding

--- OOPSConcept/Polymorphism.cs
+++ OOPSConcept/Polymorphism.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace OOPSConcept
 {
-    public class Polymorphism
+
+    // By Method Overridng
+    public class Company
     {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal ProductAmount { get; set; }
+
+        public virtual decimal CalculateDiscount()
+        {
+            return ProductAmount;
+            
+        }
+    }
+
+    public class Employees : Company
+    {
+        public override decimal CalculateDiscount()
+        {
+            return ProductAmount - 10;
+        }
+    }
+
+    public class Managers : Company
+    {
+        public override decimal CalculateDiscount()
+        {
+            return ProductAmount - 20;
+        }
+    }
+
+    // By method OverLoading
+    
+    public class Hospital
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public decimal TotalSalary()
+        {
+            return Salary;
+        }
+        public decimal TotalSalary(decimal nurseIncentive)
+        {
+            return Salary + nurseIncentive;
+        }
+
+        public decimal TotalSalary(decimal doctorIncentive, decimal leaves)
+        {
+            return Salary + doctorIncentive - leaves;
+        }
+
     }
 }
--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -51,7 +51,7 @@
             //supervisor.FirstName = "Rishi";
             //supervisor.LastName = "Kapoor";
             //Console.WriteLine("Supervisor id is: " + supervisor.GetSupervisorId + " and full name is " + supervisor.GetFullName()+"\n");
-            
+
 
             //// Interface implemnetation (multiple inheritance)
 
@@ -75,22 +75,49 @@
 
             // Abstraction
 
-            SadCustomer obj1 = new SadCustomer();
-            obj1.Name = "Sumit";
-            obj1.Address = "Bangalore";
-            obj1.productAmount = 100;
+            //SadCustomer obj1 = new SadCustomer();
+            //obj1.Name = "Sumit";
+            //obj1.Address = "Bangalore";
+            //obj1.productAmount = 100;
 
-            Console.WriteLine("Name = " + obj1.Name + "\nAddress = " + obj1.Address + "\nProductAmount = " + obj1.productAmount +
-                                        "\nProductAmount after discount = " + obj1.CalculateDiscount() + "\nGreetingMessage = " + obj1.GreetingMessage());
+            //Console.WriteLine("Name = " + obj1.Name + "\nAddress = " + obj1.Address + "\nProductAmount = " + obj1.productAmount +
+            //                            "\nProductAmount after discount = " + obj1.CalculateDiscount() + "\nGreetingMessage = " + obj1.GreetingMessage());
 
-            HappyCustomer obj2 = new HappyCustomer();
-            obj2.Name = "Suresh";
-            obj2.Address = "Bangalore";
-            obj2.productAmount = 100;
-            Console.WriteLine("\nName = " + obj2.Name + "\nAddress = " + obj2.Address + "\nProductAmount = " + obj2.productAmount +
-                                        "\nProductAmount after discount = " + obj2.CalculateDiscount() + "\nGreetingMessage = " + obj2.GreetingMessage());
+            //HappyCustomer obj2 = new HappyCustomer();
+            //obj2.Name = "Suresh";
+            //obj2.Address = "Bangalore";
+            //obj2.productAmount = 100;
+            //Console.WriteLine("\nName = " + obj2.Name + "\nAddress = " + obj2.Address + "\nProductAmount = " + obj2.productAmount +
+            //                            "\nProductAmount after discount = " + obj2.CalculateDiscount() + "\nGreetingMessage = " + obj2.GreetingMessage());
+
+            // -- Polymorphism by method overriding
 
 
+            //Company com = new Employees();
+            //com = new Managers();
+
+            // object having more than one form here.
+
+            Company com = new Employees();
+            com.Name = "Rahul";
+            com.Address = "Bangalore";
+            com.ProductAmount = 100;
+
+            Console.WriteLine("Emp Name = "+com.Name+"\nEmp Address = "+com.Address+"\nCompany Product Amount = "+com.ProductAmount
+                +"\nProduct Amount After distcount = "+com.CalculateDiscount()+"\n");
+
+            Company man = new Managers();
+            man.Name = "Rituraj";
+            man.Address = "Bangalore";
+            man.ProductAmount = 100;
+
+            Console.WriteLine("Emp Name = " + man.Name + "\nEmp Address = " + man.Address + "\nCompany Product Amount = " + man.ProductAmount
+                + "\nProduct Amount After distcount = " + man.CalculateDiscount());
+
+            Hospital obj = new Hospital();
+            obj.Salary = 100;
+
+            Console.WriteLine("\nTotal Salary for Nurse is "+obj.TotalSalary(100)+"\nTotal Salary for Doctor is : "+obj.TotalSalary(100,50));
         }
     }
 }
