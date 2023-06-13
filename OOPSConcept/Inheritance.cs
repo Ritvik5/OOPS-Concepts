c337f81d3a75ee37baee1472008b2786792e79af
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-13 17:31:06 +0530

    [Ritvik]Add: Single Level Inheritance

---
+++ OOPSConcept/Inheritance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    // -- Single Level Inheritance
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+        public string CompanyName { get; set; }
+    }
+
+}
--- OOPSConcept/Program.cs
+++ OOPSConcept/Program.cs
@@ -5,19 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Object Oriented Programing!!!\n");
-            Patient patient = new Patient();
-            patient.GetName = "Ajay";
-            patient.GetAddress = "127.0.0.1";
+            //Patient patient = new Patient();
+            //patient.GetName = "Ajay";
+            //patient.GetAddress = "127.0.0.1";
 
-            Console.WriteLine("Name of the Patient is: " + patient.GetName + " and Address is: " + patient.GetAddress + "\n");
+            //Console.WriteLine("Name of the Patient is: " + patient.GetName + " and Address is: " + patient.GetAddress + "\n");
 
-            BankAccount obj = new BankAccount(1000);
-            obj.Deposit(2000);
+            //BankAccount obj = new BankAccount(1000);
+            //obj.Deposit(2000);
 
-            Console.WriteLine("Balance after deposit is: " + obj.GetBalance() + "\n");
+            //Console.WriteLine("Balance after deposit is: " + obj.GetBalance() + "\n");
 
-            obj.Withdraw(500);
-            Console.WriteLine("Balance after withdraw is: " + obj.GetBalance());
+            //obj.Withdraw(500);
+            //Console.WriteLine("Balance after withdraw is: " + obj.GetBalance());
+
+            /* Inheritansce -- Derived class has all the properties , methods of the parent class */
+
+            // -- Single Level Inheritance
+
+            Employee employee = new Employee();
+            employee.FirstName = "Ankur";
+            employee.LastName = "Bhardhwaj";
+            employee.EmployeeId = 1;
+            employee.CompanyName = "Radha Textiles";
+            Console.WriteLine("Employee Full Name is " + employee.FirstName + " " + employee.LastName +
+                            " and employee ID is " + employee.EmployeeId + " for the company " + employee.CompanyName + ".\n");
+
             Console.Read();
         }
     }
